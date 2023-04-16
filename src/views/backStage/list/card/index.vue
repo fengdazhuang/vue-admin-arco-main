f2434e635eea1632bfa3a1a40a4bf02e09aa8f17
地中海强者 <101050485+Overmatch-of-The-Mediterranean@users.noreply.github.com>
2023-04-16 12:20:35 +0800

    4-16

--- src/views/backStage/list/card/index.vue
+++ src/views/backStage/list/card/index.vue
@@ -188,45 +188,49 @@ export default defineComponent({
       // console.log('data',data.value)
     }
 
-    const uploadCover = (e)=> {
-      var me = ctx1.ctx;
-
-      let f = inputPic.value.files[0];
-
-      let multiForm = new FormData() ; 		//创建一个form对象
-      multiForm.append('files', f, f.name);  	//append 向form表单添加数据
-
-      // 请求后端获得最新数据
-      var fsServerUrl = 'http://localhost:8009';
-      axios.defaults.withCredentials = true;
-      var fileServer = fsServerUrl + '/file/uploadFiles';
-
-      axios.post(
-          fileServer,
-          multiForm,
-          {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            }
-          })
-          .then(res => {
-            console.log('resImg',res)
-            if (res.code === 200) {
-              var imagesList = res.data;
-              if (imagesList.length < 1) {
-                alert("张图片上传失败，请保证图片不能为空，并且符合 jpg/png/jpeg 的后缀格式！");
-              } else {
-                imgSrc.value = imagesList[0];
-                form.picture = imagesList[0]
-                isImg.value = true
-
-              }
-            } else {
-              alert(res.data.msg);
-            }
-          });
-    }
-
+    // const uploadCover = (e)=> {
+    //   var me = ctx1.ctx;
+    //
+    //   let f = inputPic.value.files[0];
+    //
+    //   let multiForm = new FormData() ; 		//创建一个form对象
+    //   multiForm.append('files', f, f.name);  	//append 向form表单添加数据
+    //
+    //   // 请求后端获得最新数据
+    //   var fsServerUrl = 'http://localhost:8009';
+    //   axios.defaults.withCredentials = true;
+    //   var fileServer = fsServerUrl + '/file/uploadFiles';
+    //
+    //   axios.post(
+    //       fileServer,
+    //       multiForm,
+    //       {
+    //         headers: {
+    //           'Content-Type': 'multipart/form-data',
+    //         }
+    //       })
+    //       .then(res => {
+    //         console.log('resImg',res)
+    //         if (res.code === 200) {
+    //           var imagesList = res.data;
+    //           if (imagesList.length < 1) {
+    //             alert("张图片上传失败，请保证图片不能为空，并且符合 jpg/png/jpeg 的后缀格式！");
+    //           } else {
+    //             imgSrc.value = imagesList[0];
+    //             form.picture = imagesList[0]
+    //             isImg.value = true
+    //
+    //           }
+    //         } else {
+    //           alert(res.data.msg);
+    //         }
+    //       });
+    // }
+      const uploadCover = (e)=> {
+          imgSrc.value='@/assets/images/img1.jpg'
+          form.picture = '@/assets/images/img1.jpg'
+          isImg.value = true
+      }
     getComCategorys()
     return {
       // getComCategorys
